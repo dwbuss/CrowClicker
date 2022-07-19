c36daefff02f5f847773a1c472cf4b08f6a62bf7
Jeff Brekke <jeff@byteangler.com>
2022-07-18 22:03:40 -0500

    Fixed so Weather button updates form, cleaned up fields

--- app/src/main/java/com/example/clicker/ContactType.java
+++ app/src/main/java/com/example/clicker/ContactType.java
@@ -2,9 +2,9 @@ package com.example.clicker;
 
 public enum ContactType {
 
+    CATCH  ( R.raw.cc_nice2,    new int[] { R.raw.cc_nice3, R.raw.g_bitch}),
     CONTACT( R.raw.cc_yousuck2, new int[] { R.raw.f_upped, R.raw.cc_yousuck3 }),
-    FOLLOW ( R.raw.cc_follow2,  new int[] { R.raw.wtf_lookin, R.raw.cc_follow3}),
-    CATCH  ( R.raw.cc_nice2,    new int[] { R.raw.cc_nice3, R.raw.g_bitch});
+    FOLLOW ( R.raw.cc_follow2,  new int[] { R.raw.wtf_lookin, R.raw.cc_follow3});
 
     private final int friendly;
     private final int[] bites;

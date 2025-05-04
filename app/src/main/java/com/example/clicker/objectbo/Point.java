9bf47c3bffa683ca4482d39ab47838d486d9f863
Jeff Brekke <jeff@byteangler.com>
2025-05-04 09:51:51 -0500

    Latest updates and fixed a bug in the text message when no length is entered.

--- app/src/main/java/com/example/clicker/objectbo/Point.java
+++ app/src/main/java/com/example/clicker/objectbo/Point.java
@@ -493,11 +493,17 @@ public final class Point implements Parcelable {
     public String getMessage(String time) {
         final String format = "%1$s %2$s%3$s %4$s.%n%5$s%n%8$s%nhttps://maps.google.com/maps?q=%6$f,%7$f";
         if (contactType.equalsIgnoreCase(ContactType.CATCH.toString())) {
-            return String.format("%1$s %2$s a %3$s\" %10$s on%4$s %5$s.%n%6$s%n%9$s%nhttps://maps.google.com/maps?q=%7$f,%8$f", getName().trim(), ContactType.CATCH.getMessageFragment(), getFishSize().trim(), getSinglularModifier(getBait().trim()), getBait().trim(), getNotes().trim(), getLat(), getLon(), time, getSpecies().trim());
+            return String.format("%1$s %2$s a %3$s%10$s on%4$s %5$s.%n%6$s%n%9$s%nhttps://maps.google.com/maps?q=%7$f,%8$f", getName().trim(), ContactType.CATCH.getMessageFragment(), getFishSizeFormatted(), getSinglularModifier(getBait().trim()), getBait().trim(), getNotes().trim(), getLat(), getLon(), time, getSpecies().trim());
         } else if (contactType.equalsIgnoreCase(ContactType.FOLLOW.toString()))
             return String.format(format, getName().trim(), ContactType.FOLLOW.getMessageFragment(), getSinglularModifier(getBait().trim()), getBait().trim(), getNotes().trim(), getLat(), getLon(), time);
         else
             return String.format(format, getName().trim(), ContactType.CONTACT.getMessageFragment(), getSinglularModifier(getBait().trim()), getBait().trim(), getNotes().trim(), getLat(), getLon(), time);
+    }
+
+    private String getFishSizeFormatted() {
+        if (fishSize.trim().isEmpty())
+            return "";
+        return fishSize.trim()+"\" ";
     }
 
     public List<List<Object>> getSheetBody() {
--- app/src/test/java/com/example/clicker/PointTest.java
+++ app/src/test/java/com/example/clicker/PointTest.java
@@ -66,6 +66,19 @@ public class PointTest {
     }
 
     @Test
+    public void testGetSingluarMessageCatchWithoutLength() throws ParseException {
+        String csvRecord = "237\tDan\t-93.85824702680111\t49.21601569265263\t09-02-2021 09:11 AM\tCATCH\t61.5°\t\tSpoon\t\tTypical pike!\t'''''9.72 mph\tSE\t0.99\t54.06°\t1017.2 mb\t0.77\tCrow\tPike\t \t ";
+        Point point = new Point(csvRecord);
+        assertEquals("Dan caught a Pike on a Spoon.\nTypical pike!\n8:03 pm\nhttps://maps.google.com/maps?q=49.216016,-93.858247", point.getMessage("8:03 pm"));
+    }
+
+    @Test
+    public void testGetPluralMessageCatchWithNoLength() throws ParseException {
+        String csvRecord = "237\tDan\t-93.85824702680111\t49.21601569265263\t09-02-2021 09:11 AM\tCATCH\t61.5°\t\tBlades\t\tTypical pike!\t'''''9.72 mph\tSE\t0.99\t54.06°\t1017.2 mb\t0.77\tCrow\tMuskellunge\t20";
+        Point point = new Point(csvRecord);
+        assertEquals("Dan caught a Muskellunge on Blades.\nTypical pike!\n8:04 pm\nhttps://maps.google.com/maps?q=49.216016,-93.858247", point.getMessage("8:04 pm"));
+    }
+    @Test
     public void testCreatePoint() throws ParseException {
         String csvRecord = "237\tDan\t-93.85824702680111\t49.21601569265263\t09-02-2021 09:11 AM\tFOLLOW\t61.5°\t\tdusa\t\t\t'''''9.72 mph\tSE\t0.99\t54.06°\t1017.2 mb\t0.77\tCrow\t \t ";
         Point point = new Point(csvRecord);
